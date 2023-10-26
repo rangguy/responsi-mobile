35352a6a1cc2e600844dc32c085eda2c8f787d29
rangguy <ranggadwi100@gmail.com>
2023-10-26 21:10:50 +0700

    Manajemen tugas, Create, Read, Delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:manajemen_tugas/ui/tugas_page.dart';
 
 void main() {
   runApp(const MyApp());
+
+  WidgetsFlutterBinding.ensureInitialized();
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: TugasPage(),
     );
   }
 }
+
